b136308adf1a1d5256e84a5ddba6548714035dd6
Tomasz Jaworski <tomasz.jaworski98@gmail.com>
2018-02-22 16:04:21 +0100

    Change the way of using nuget

---
+++ src/PatchKit.Api/Properties/Version.cs
@@ -0,0 +1,10 @@
+﻿namespace PatchKit.Api.Properties
+{
+    internal static class Version
+    {
+        public const string Major = "1";
+        public const string Minor = "2";
+        public const string Patch = "1";
+        public const string Suffix = "";
+    }
+}
