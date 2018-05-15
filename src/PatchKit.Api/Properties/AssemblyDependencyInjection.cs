fead5cc3f82e676186d26378ed4f8ad1c055849b
Tomasz Jaworski <tomasz.jaworski98@gmail.com>
2018-05-15 13:08:03 +0200

    IOC container integration WIP

---
+++ src/PatchKit.Api/Properties/AssemblyDependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using PatchKit.Core.DependencyInjection;
+
+namespace PatchKit.Api.Properties
+{
+    public class AssemblyDependencyInjection
+    {
+        static AssemblyDependencyInjection()
+        {
+            var containerBuilder = new ContainerBuilder();
+
+            containerBuilder.AddDependency(Core.Properties.AssemblyDependencyInjection.Container);
+
+            containerBuilder.RegisterType<IBaseApiConnectionFactory, BaseApiConnectionFactory>();
+            containerBuilder.RegisterType<IApiConnection, ApiConnection>(ApiConnectionSettings.DefaultApi);
+            containerBuilder.RegisterType<IKeysApiConnection, KeysApiConnection>(ApiConnectionSettings.DefaultKeysApi);
+
+            Container = containerBuilder.Container;
+        }
+
+        public static Container Container { get; }
+    }
+}
